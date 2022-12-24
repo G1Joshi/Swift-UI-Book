039baf6e3710fed83d184b2cd68f9db73a78f597
Jeevan Joshi <joshijeevan98@gmail.com>
2022-12-24 10:19:07 +0530

    initial commit

---
+++ Swift-UI-Book/Swift_UI_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_BookApp.swift
+//  Swift-UI-Book
+//
+//  Created by Jeevan Chandra Joshi on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
